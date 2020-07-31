7fd1c2fcaa97d654d86d5d2f5922a530d9f74a1c
Dan Graur <dgraur@google.com>
2020-07-31 12:30:56 +0000

    Made some small modifications in the input pipeline and the main_retinanet.py source.

--- examples/retinanet/main_retinanet.py
+++ examples/retinanet/main_retinanet.py
@@ -5,18 +5,25 @@ import jax
 import tensorflow as tf
 
 
-def main(single_thread=True):
+def disable_gpu():
+  import os
+  os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+def main(single_thread: bool = True):
+  img_size = (224, 224, 3)
   data = read_data()
-  train_data, test_data = prepare_data(data, 2, not single_thread)
+  train_data, test_data = prepare_data(data, 2, not single_thread, img_size[:2])
   
   if single_thread:
     import train_single_thread as logic
   else: 
     import train as logic
   
-  logic.train_retinanet_model(train_data, test_data, (1000, 1000, 3), 
+  logic.train_retinanet_model(train_data, test_data, img_size, 
                               batch_size=2, try_restore=False)  
 
 
 if __name__ == '__main__':
+  disable_gpu()
   main()
