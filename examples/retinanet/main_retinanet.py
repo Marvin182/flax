fe05dfc62499e7d4325152a4229a2561a46dbf8c
Dan Graur <dgraur@google.com>
2020-07-31 14:00:08 +0000

    Made some small changes to the code.

--- examples/retinanet/main_retinanet.py
+++ examples/retinanet/main_retinanet.py
@@ -11,17 +11,25 @@ def disable_gpu():
 
 
 def main(single_thread: bool = True):
+  # Some training parameters
+  batch_size = 1
+
+  # Read the data
+  rng = jax.random.PRNGKey(0)
+  rng, input_key = jax.random.split(rng)
+  data = read_data(input_key)
+
+  # Process the images
   img_size = (224, 224, 3)
-  data = read_data()
-  train_data, test_data = prepare_data(data, 2, not single_thread, img_size[:2])
+  train_data, test_data = prepare_data(data, batch_size, not single_thread, img_size[:2])
   
   if single_thread:
     import train_single_thread as logic
   else: 
     import train as logic
   
-  logic.train_retinanet_model(train_data, test_data, img_size, 
-                              batch_size=2, try_restore=False)  
+  logic.train_retinanet_model(rng, train_data, test_data, img_size, 
+                              batch_size=batch_size, try_restore=False)  
 
 
 if __name__ == '__main__':
